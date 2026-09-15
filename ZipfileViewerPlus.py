8d82296252d70bb033c2b66b92456a8027eae872
WOOSEOK99 <woosuk00@naver.com>
2026-09-16 08:50:48 +0900

    Use searchable cheat browser tab

--- ZipfileViewerPlus.py
+++ ZipfileViewerPlus.py
@@ -3,20 +3,20 @@ import sys
 from PyQt6.QtWidgets import QApplication, QWidget, QVBoxLayout, QTabWidget
 
 from ZipfileViewer import CombinedApp
-from CheatTab import CheatTab
+from CheatTabWithBrowser import CheatTabWithBrowser
 
 
 class ZipfileViewerPlus(QWidget):
     def __init__(self):
         super().__init__()
         self.setWindowTitle("압축파일 썸네일 & 치트 매칭 매니저")
-        self.resize(1380, 760)
+        self.resize(1380, 900)
 
         layout = QVBoxLayout(self)
         tabs = QTabWidget()
 
         self.thumbnail_tab = CombinedApp()
-        self.cheat_tab = CheatTab()
+        self.cheat_tab = CheatTabWithBrowser()
 
         tabs.addTab(self.thumbnail_tab, "썸네일")
         tabs.addTab(self.cheat_tab, "치트")
